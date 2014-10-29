5f1df1c93bb04e0726da24a7230d0100608c18fd
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-29 11:12:32 +0100

    primeiro if

---
+++ src/boletin0/pkg1/Boletin01.java
@@ -0,0 +1,22 @@
+
+package boletin0.pkg1;
+
+
+public class Boletin01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double num1,num2;
+      Comparar obx=new Comparar();
+      num1=obx.pedirDato();
+      num2=obx.pedirDato();
+      obx.compararSimple(num1,num2);
+      
+        
+        
+        }
+    }
+    
+
